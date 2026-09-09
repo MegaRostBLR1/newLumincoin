8417321dc364fd5b68a8ba0435fa487d513e631c
Ростислав Якубовский <110860900+MegaRostBLR1@users.noreply.github.com>
2026-09-09 15:52:46 +0300

    fix: show operation deletion errors

--- src/components/generals/deleteGeneralElement.js
+++ src/components/generals/deleteGeneralElement.js
@@ -1,5 +1,6 @@
 import {AuthTokens} from "../utils/auth-utils.js";
 import {Response} from "../utils/response-utils.js";
+import {ErrorUtils} from "../utils/error-utils.js";
 
 export class DeleteGeneralElement {
     constructor(openNewRouteAutomatic, urlRequest, url) {
@@ -8,12 +9,15 @@ export class DeleteGeneralElement {
         this.url = url;
         this.deleteBtn = document.getElementById('deleteBtn');
         this.cancelBtn = document.getElementById('cancelBtn');
+        this.errorElement = document.getElementById('server-error');
 
         if (this.deleteBtn) this.deleteBtn.onclick = this.deleteElement.bind(this);
         if (this.cancelBtn) this.cancelBtn.onclick = this.cancelDelete.bind(this);
     }
 
     async deleteElement() {
+        if (this.errorElement) this.errorElement.innerText = '';
+
         const generalElementId = localStorage.getItem('idRowGenerals');
         if (!generalElementId) {
             await this.openNewRouteAutomatic(this.url);
@@ -34,6 +38,7 @@ export class DeleteGeneralElement {
 
         if (!result || result.error) {
             console.error('Ошибка удаления операции:', result);
+            ErrorUtils.show(result, this.errorElement, 'удалить операцию');
             return;
         }
 
